7271df3a4b254c8dc69c545e42b85183073ce9d8
iqrahussein <iqramohamed600@gmail.com>
2026-09-07 12:54:30 +0300

    node  js exercise 1

---
+++ note js exercises1/index.js
@@ -0,0 +1,41 @@
+const express=require("express")
+
+const app =express();
+app.use(express.json());
+
+let books = [
+  { id: 1, title: 'Atomic Habits', author: 'James Clear' },
+  { id: 2, title: 'Deep Work', author: 'Cal Newport' }
+];
+
+app.get("/",(req,res)=>{
+    res.json( books)
+})
+
+app.get('/books/:id',(req,res)=>{
+    const book=books.find(b=>req.params.id==b.id)
+    res.json(book)
+})
+
+app.post('/books',(req,res)=>{
+    const newUsers= {
+     id:books.length+1,
+     title:req.body.title
+    }
+    books.push(newUsers)
+    res.status(201).json(newUsers)
+})
+app.delete('/books/:id',(req,res)=>{
+    books =books.filter(b => b.id !=req.params.id)
+    res.send("user deleted")
+})
+app.put('/books/:id',(req,res)=>{
+    const book=books.find(b => b.id==req.params.id)
+    if(!book) return res.status(404).send("not founs")
+    book.title=req.body.title
+    res.json(book)
+})
+
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
